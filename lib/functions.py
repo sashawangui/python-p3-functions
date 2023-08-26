06a92d3cc791ee8c8c0b02201cb9238451b0e51d
sasha wangui <sasha.iluku@gmail.com>
2023-08-26 14:33:11 +0300

    done

--- lib/functions.py
+++ lib/functions.py
@@ -2,15 +2,26 @@
 
 def greet_programmer():
     pass
+    print ("Hello, programmer!")
 
 def greet(name):
     pass
+    print ("Hello, " + name + "!")
 
 def greet_with_default(name="programmer"):
     pass
+    print ("Hello, " + name + "!")
 
 def add(num1, num2):
     pass
+    return num1 + num2
+
 
 def halve(number):
     pass
+    if isinstance(number, (int, float)):
+        return number / 2
+    else:
+        return None
+
+
